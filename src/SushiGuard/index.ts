09ce383a00ebf3dcd7f4b10d96f0b1f7e063af12
sam bacha <sam@manifoldfinance.com>
2022-03-26 20:31:21 -0700

    chore(repo): cleanup

--- src/SushiGuard/index.ts
+++ src/SushiGuard/index.ts
@@ -5,9 +5,9 @@ import { JsonRpcResponse } from '../JsonRpc/index';
  *
  * - UNCHECKED -> Tx status has not been checked and there's no information about it.
  * - PROCESSING -> Tx checks are in place until a resolution happens: OK, INDETERMINATE, ERROR.
- * - OK -> Relay received the Tx && all downstream miners accepted without complains && tx mined sucessfully
+ * - OK -> Relay received the Tx && all downstream miners accepted without complains && tx mined successfully
  * - INDETERMINATE -> Relay received correctly the Tx && at least one miner accepted the TX && TX potentially mineable
- * - ERROR -> Relay hasen't received the TX || none of the miners accepted the Tx || Tx was not mined sucessfully
+ * - ERROR -> Relay hasn't received the TX || none of the miners accepted the Tx || Tx was not mined successfully
  *
  */
 
